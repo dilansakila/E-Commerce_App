15384495339033a186d5c8b12dd1a95146e028d1
dilansakila <dilansakila99@gmail.com>
2024-07-05 18:41:08 +0530

    add modules

---
+++ ecommerce_website/src/comp/nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FaBeer } from "react-icons/fa";
+
+const nav = () => {
+  return (
+    <>
+      <div className='header'>
+        <div className='top_header'>
+            <div className='icon'>
+
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default nav

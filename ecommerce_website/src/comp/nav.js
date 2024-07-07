01f1fa2970f96eab4542e32e52118a307d696943
dilansakila <dilansakila99@gmail.com>
2024-07-07 10:57:17 +0530

    updated nav

--- ecommerce_website/src/comp/nav.js
+++ ecommerce_website/src/comp/nav.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { MdLocalShipping } from "react-icons/md";
+import { CiSearch } from "react-icons/ci";
 import './nav.css'
 
 
@@ -14,6 +15,15 @@ const nav = () => {
             <div className="info">
               <p>Free Shipping When Shopping upto $1000</p>
             </div>
+        </div>
+        <div className='mid_header'>
+          <div className='logo'>
+            <img src=""></img>
+          </div>
+          <div className='search_box'>
+            <input type='text' value='' placeholder='search'></input>
+            <button><CiSearch /></button>
+          </div>
         </div>
       </div>
     </>
